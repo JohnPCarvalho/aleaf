ea6b3f60a2e2d1477b0efd99edb0a44175ad3282
JohnPCarvalho <johnnypcarvalho@gmail.com>
2021-05-01 02:01:49 -0300

    Seed file for the migration with mix ecto.reset

--- priv/repo/migrations/20210501034508_create_entities.exs
+++ priv/repo/migrations/20210501034508_create_entities.exs
@@ -5,11 +5,11 @@ defmodule Arvore.Repo.Migrations.CreateEntities do
     create table(:entities) do
       add :name, :string
       add :entity_type, :string
-      add :inep, :string
+      add :inep, :string, unique: true
       add :parent_id, references(:entities)
 
       timestamps()
     end
-
+    create unique_index(:entities, :inep)
   end
 end
--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,11 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+IO.puts("Adding a couple of items...")
+
+Arvore.Leaf.create_entity(%{name: "Colégio Objetivo", entity_type: "network", inep: "123", parent_id: nil })
+Arvore.Leaf.create_entity(%{name: "SESI", entity_type: "network", inep: "321", parent_id: nil })
+Arvore.Leaf.create_entity(%{name: "Colégio Objetivo Monte Alto", entity_type: "school", inep: "123-300", parent_id: 1 })
+Arvore.Leaf.create_entity(%{name: "Colégio Objetivo Rio de Janeiro", entity_type: "school", inep: "123-2", parent_id: 1 })
+Arvore.Leaf.create_entity(%{name: "SESI - Unidade São Paulo", entity_type: "school", inep: "321-1", parent_id: 2 })
